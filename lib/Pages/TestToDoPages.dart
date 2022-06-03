eeaff6a4b48ab3564504dd92fb80666880db3b83
James Fernando <=jamesfernando1311@gmail.com>
2022-06-03 23:12:31 +0700

    sql create local DB

--- lib/Pages/TestToDoPages.dart
+++ lib/Pages/TestToDoPages.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:proj1/model/todotile.dart';
 import 'package:proj1/pages/Helper/basiccard.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
 
 class TestToDoPages extends StatelessWidget {
   final TextEditingController _controller = TextEditingController();
@@ -70,3 +76,57 @@ class TestToDoPages extends StatelessWidget {
     );
   }
 }
+
+class todotilesql {
+  final int? id;
+  final String text;
+
+  todotilesql({this.id, required this.text});
+  factory todotilesql.fromMap(Map<String, dynamic>json)=>new todotilesql(
+    id: json['id'],
+    text: json['text']
+  );
+
+  Map<String, dynamic>toMap(){
+    return{
+      'id':id,
+      'text':text
+    }
+  }
+
+}
+
+class DatabaseHelper{
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = new DatabaseHelper._privateConstructor();
+  static Database?_database;
+  Future <Database> get database async=>_database??=await _initDatabase();
+
+  Future <Database> _initDatabase() async{
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path=join(documentsDirectory.path, 'todolist1.db');
+    return await openDatabase(
+      path,
+      version:1,
+      onCreate:_onCreate,
+    );
+  }
+
+  Future _onCreate(Database db, int version) async{
+    await db.execute('''
+      CREATE TABLE todolist(
+        id INTEGER PRIMARY KEY,
+        name TEXT
+      )
+    ''');
+  }
+
+  Future <List<todotilesql>> getToDoList()async{
+      Database db =await instance.database;
+      var todotiles = await db.query('todotiles');
+  }
+
+
+}
+
+
